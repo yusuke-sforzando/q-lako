8c6270fad5b418c9beab92c17fc9ccb41c1b9095
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-15 02:34:16 +0900

    :sparkles: Add settings.py for read api key on .env

---
+++ settings.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+airtable_base_id = os.getenv("airtable_base_id")
+airtable_api_key = os.getenv("airtable_api_key")
+amazon_partner_tag = os.getenv("amazon_partner_tag")
+amazon_access_key = os.getenv("amazon_access_key")
+amazon_secret_key = os.getenv("amazon_secret_key")
