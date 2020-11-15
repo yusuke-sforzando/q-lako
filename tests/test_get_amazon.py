6307ad45b94d353e01c5d3075a0f7597bd90b6d2
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-16 05:18:45 +0900

    :memo: Add comment for test_get_amazon.py

--- tests/test_get_amazon.py
+++ tests/test_get_amazon.py
@@ -2,20 +2,28 @@ from get_amazon import AmazonListGetter
 
 
 def test_search_amazon_title():
+    """Testing when the equipment name is entered."""
+
     amazon_search_title = AmazonListGetter("Alexa").amazon_list
     assert amazon_search_title
 
 
 def test_search_amazon_asin():
+    """Testing when the ASIN code is entered."""
+
     amazon_search_asin = AmazonListGetter("B08BRBFSDR").amazon_list
     assert amazon_search_asin
 
 
 def test_search_amazon_isbn():
+    """Testing when the ISBN code is entered."""
+
     amazon_search_isbn = AmazonListGetter("9784839966607").amazon_list
     assert amazon_search_isbn
 
 
 def test_search_amazon_no():
+    """Testing when a non-existent value is entered."""
+
     amazon_search_no = AmazonListGetter("9784839966607asadAASSSSSS").amazon_list
     assert not amazon_search_no
