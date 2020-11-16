f7fe8f6f8743358a3e8c521e4576e93daa76dfc2
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-17 01:44:27 +0900

    :+1: Improve get_amazon.py

--- get_amazon.py
+++ get_amazon.py
@@ -2,43 +2,40 @@
 
 from amazon.paapi import AmazonAPI
 from amazon.paapi import AmazonException
-
 import os
 
 
-class FetchAmazon:
+class AmazonGetter:
     """Get search results from Amazon."""
 
-    def __init__(self, search_word: str, item_count=3):
+    def __init__(self):
         """Initialize GetAmazonList."""
-        self.amazon_list = []
-        self.search_word = search_word
-        self.item_count = item_count
         self.amazon = AmazonAPI(os.getenv("amazon_access_key"), os.getenv("amazon_secret_key"),
                                 os.getenv("amazon_partner_tag"), "JP")
-        self.get_search_list()
 
-    def get_search_list(self):
+    def get_search_list(self, keyword: str, item_count=30):
         """Get the search list.
 
             Retrieves a list of search results with any URL,
             product name and ASIN/ISBN code as arguments.
+        Args:
+            keyword (str): The product name or code you want to search for on Amazon.
+            item_count (int): The number of items you want to search for at a time.
 
         Returns:
-            products (list): List of Amazon items (from 1 to 30).
+            products (list): List of Amazon items.
         """
 
         try:
-            self.products = self.amazon.search_items(item_count=self.item_count, keywords=self.search_word)
-            for product in self.products["data"]:
-                title = product.item_info.title.display_value
-                url = product.detail_page_url
-                try:
-                    image_url = product.images.primary.medium.url
-                    self.amazon_list.append({"Title": title, "url": url, "image_url": image_url})
-                except AttributeError as e:
-                    print("image_url is not exist", e)
-                    self.amazon_list.append({"Title": title, "url": url, "image_url": ""})
+            products = self.amazon.search_items(keywords=keyword, item_count=item_count)
         except AmazonException as e:
             print("No results found for your request", e)
-            self.amazon_list = []
+            return []
+
+        product_list = []
+        for product in products["data"]:
+            product = product.to_dict()
+            image_url = product["images"]["primary"]["large"]["url"] if product.get("images") else "None"
+            product_list.append({"ASIN": product["asin"], "Title": product["item_info"]["title"]
+                                 ["display_value"], "url": product["detail_page_url"], "image_url": image_url})
+        return product_list
--- tests/test_get_amazon.py
+++ tests/test_get_amazon.py
@@ -1,29 +1,36 @@
-from get_amazon import FetchAmazon
+from get_amazon import AmazonGetter
 
 
 def test_search_amazon_with_title():
     """Testing when the equipment name is entered."""
 
-    amazon_search_title = FetchAmazon("Alexa").amazon_list
+    amazon_search_title = AmazonGetter().get_search_list("Alexa")
     assert amazon_search_title
 
 
 def test_search_amazon_with_asin():
     """Testing when the ASIN code is entered."""
 
-    amazon_search_asin = FetchAmazon("B08BRBFSDR").amazon_list
+    amazon_search_asin = AmazonGetter().get_search_list("B08BRBFSDR")
     assert amazon_search_asin
 
 
 def test_search_amazon_with_isbn():
     """Testing when the ISBN code is entered."""
 
-    amazon_search_isbn = FetchAmazon("9784839966607").amazon_list
+    amazon_search_isbn = AmazonGetter().get_search_list("9784839966607")
     assert amazon_search_isbn
+
+
+def test_search_amazon_empty():
+    """Testing when an empty keyword is entered."""
+
+    amazon_search_empty = AmazonGetter().get_search_list("")
+    assert not amazon_search_empty
 
 
 def test_search_amazon_no_exist():
     """Testing when a non-existent value is entered."""
 
-    amazon_search_no = FetchAmazon("").amazon_list
+    amazon_search_no = AmazonGetter().get_search_list("0123456781011121314")
     assert not amazon_search_no
