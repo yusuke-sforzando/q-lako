6a5327b959fbb65af9f13aac40ca80424ce22b0d
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-16 15:16:10 +0900

    :fire: Remove api_keys.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
+
+airtable_base_id = os.getenv("airtable_base_id")
+airtable_api_key = os.getenv("airtable_api_key")
+amazon_partner_tag = os.getenv("amazon_partner_tag")
+amazon_access_key = os.getenv("amazon_access_key")
+amazon_secret_key = os.getenv("amazon_secret_key")
 
 if os.getenv("GAE_ENV", "").startswith("standard"):
     """ Production in GAE """
@@ -27,3 +36,5 @@ else:
         dashboard.bind(app)
     except ImportError as ie:
         app.logger.warning(f"{ie}")
+
+print(airtable_api_key)
