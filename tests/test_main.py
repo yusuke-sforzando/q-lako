e1ca30291ff3615533d07cda6fb9bdf0495835e5
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-18 16:30:42 +0900

    :memo: Rename amazon to amazon_api_client

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from __init__ import amazon
+from __init__ import amazon_api_client
 from main import app
 import os
 
@@ -28,26 +28,26 @@ def test_api_keys():
 def test_search_amazon_with_title():
     """Testing when the equipment name is entered."""
 
-    amazon_search_title = amazon.search_products(keywords="PS5")
+    amazon_search_title = amazon_api_client.search_products(keywords="PS5")
     assert amazon_search_title
 
 
 def test_search_amazon_with_asin():
     """Testing when the ASIN code is entered."""
 
-    amazon_search_asin = amazon.search_products(keywords="B08BRBFSDR")
+    amazon_search_asin = amazon_api_client.search_products(keywords="B08BRBFSDR")
     assert amazon_search_asin
 
 
 def test_search_amazon_with_isbn():
     """Testing when the ISBN code is entered."""
 
-    amazon_search_isbn = amazon.search_products(keywords="9784839966607")
+    amazon_search_isbn = amazon_api_client.search_products(keywords="9784839966607")
     assert amazon_search_isbn
 
 
 def test_search_amazon_no_product():
     """Testing when a non-existent value is entered."""
 
-    search_no_exist = amazon.search_products(keywords="01234567891011121314")
+    search_no_exist = amazon_api_client.search_products(keywords="01234567891011121314")
     assert not search_no_exist
