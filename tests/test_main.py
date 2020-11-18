dedec3bd021e29a72f028e7c744987804bc5b043
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-18 15:13:07 +0900

    :+1: Improve test_main for amazon test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
-import os
-
 import pytest
 
+from __init__ import amazon
 from main import app
+import os
 
 
 @pytest.fixture
@@ -23,3 +23,31 @@ def test_api_keys():
     assert os.getenv("amazon_partner_tag")
     assert os.getenv("amazon_access_key")
     assert os.getenv("amazon_secret_key")
+
+
+def test_search_amazon_with_title():
+    """Testing when the equipment name is entered."""
+
+    amazon_search_title = amazon.search_products(keywords="B08BRBFSDR")
+    assert amazon_search_title
+
+
+def test_search_amazon_with_asin():
+    """Testing when the ASIN code is entered."""
+
+    amazon_search_asin = amazon.search_products(keywords="B08BRBFSDR")
+    assert amazon_search_asin
+
+
+def test_search_amazon_with_isbn():
+    """Testing when the ISBN code is entered."""
+
+    amazon_search_isbn = amazon.search_products(keywords="9784839966607")
+    assert amazon_search_isbn
+
+
+def test_search_amazon_no_product():
+    """Testing when a non-existent value is entered."""
+
+    search_no_exist = amazon.search_products(keywords="01234567891011121314")
+    assert not search_no_exist
