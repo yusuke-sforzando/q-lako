0eefc681e6378737e4b0d5a12453dd823024fda5
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-21 03:19:11 +0900

    :sparkles: Improve main.py by session

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # !/usr/bin/env python3
 
-from flask import render_template, request
+from flask import render_template, request, session
 
 from __init__ import app
+
+import os
+app.secret_key = os.getenv("airtable_api_key")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,10 +17,21 @@ def index():
         "subtitle": template_filename,
         "message": f"This is {template_filename}.",
         "title": "備品・書籍の登録",
-        "sub_title": "キーワード、ISBNコード、ASINコードのいずれかを入力してください",
-        "keyword": request.form.get("input_keyword")
+        "sub_title": "キーワード、ISBNコード、ASINコードのいずれかを入力してください"
     }
+    session["keyword"] = request.form.get("input_keyword")
     return render_template(template_filename, **context_dict)
+
+
+@ app.route("/search-result", methods=["GET"])
+def search_result():
+    app.logger.info("search_result(): GET /")
+    template_filename = "search-result.html"
+    context_dict = {
+        "subtitle": template_filename,
+        "message": f"This is {template_filename}."
+    }
+    return render_template(template_filename, **context_dict, keyword=session["keyword"])
 
 
 if __name__ == "__main__":
