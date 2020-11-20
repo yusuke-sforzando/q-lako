8886f239d51f9972752b4f3abda40cf658b4fb5f
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-20 23:36:28 +0900

    :sparkles: Improve main.py and index.html by form.get

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # !/usr/bin/env python3
 
-from flask import render_template
+from flask import render_template, request
 
 from __init__ import app
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     app.logger.info("index(): GET /")
     template_filename = "index.html"
+
     context_dict = {
         "subtitle": template_filename,
-        "message": f"This is {template_filename}."
+        "message": f"This is {template_filename}.",
+        "title": "備品・書籍の登録",
+        "sub_title": "キーワード、ISBNコード、ASINコードのいずれかを入力してください",
+        "keyword": request.form.get("input_keyword")
     }
     return render_template(template_filename, **context_dict)
 
