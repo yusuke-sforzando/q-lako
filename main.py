75529d3538f13a61cb6452f018167dbe08d0682a
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-21 16:57:22 +0900

    :+1: Improve main.py for use context_dict

--- main.py
+++ main.py
@@ -5,26 +5,24 @@ from flask import render_template, request
 from __init__ import app
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
     app.logger.info("index(): GET /")
     return render_template("index.html")
-
-
-@app.route("/search-result?input_keyword=")
-def search_result_direct_access():
-    app.logger.info("search_result_direct_access(): GET /")
-    return "TOPページに戻ってキーワードを入力してください"
 
 
 @app.route("/search-result", methods=["GET"])
 def search_result():
     app.logger.info("search_result(): GET /")
     keyword = request.args.get("input_keyword", "")
+    context_dict = {
+        "keyword": request.args.get("input_keyword", ""),
+        "message": "TOPページに戻ってキーワードを入力してください"
+    }
     if keyword:
-        return render_template("search-result.html", keyword=keyword)
+        return render_template("search-result.html", **context_dict)
     else:
-        return "TOPページに戻ってキーワードを入力してください"
+        return context_dict["message"]
 
 
 if __name__ == "__main__":
