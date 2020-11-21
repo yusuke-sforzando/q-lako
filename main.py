4e5f0c492b88b2f7ba33720f65027a3edea4e6b8
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-21 15:37:49 +0900

    :+1: Imrove main.py for test_main.py

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 # !/usr/bin/env python3
 
-from flask import render_template, request, session
+from flask import render_template, request
 
 from __init__ import app
-import os
-app.secret_key = os.getenv("app_secret_key")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     app.logger.info("index(): GET /")
-    template_filename = "index.html"
+    return render_template("index.html")
 
-    context_dict = {
-        "subtitle": template_filename,
-        "message": f"This is {template_filename}.",
-        "title": "備品・書籍の登録",
-        "sub_title": "キーワード、ISBNコード、ASINコードのいずれかを入力してください"
-    }
-    session["keyword"] = request.form.get("input_keyword")
-    return render_template(template_filename, **context_dict)
+
+@app.route("/search-result?input_keyword=")
+def search_result_direct_access():
+    app.logger.info("search_result_direct_access(): GET /")
+    return "TOPページに戻ってキーワードを入力してください"
 
 
 @ app.route("/search-result", methods=["GET"])
 def search_result():
     app.logger.info("search_result(): GET /")
     template_filename = "search-result.html"
-    context_dict = {
-        "subtitle": template_filename,
-        "message": f"This is {template_filename}."
-    }
-    return render_template(template_filename, **context_dict, keyword=session["keyword"])
+    keyword = request.args.get("input_keyword", "")
+    if keyword:
+        return render_template(template_filename, keyword=keyword)
+    else:
+        return "TOPページに戻ってキーワードを入力してください"
 
 
 if __name__ == "__main__":
