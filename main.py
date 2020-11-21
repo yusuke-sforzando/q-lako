c8a4c1291635eaf33a472b6c31ef66673465a769
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-21 16:45:46 +0900

    :+1: Add search_product

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 # !/usr/bin/env python3
 
-from flask import render_template
+from flask import render_template, request
 
-from __init__ import app
+from __init__ import app, amazon_api_client
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     app.logger.info("index(): GET /")
-    template_filename = "index.html"
-    context_dict = {
-        "subtitle": template_filename,
-        "message": f"This is {template_filename}."
-    }
-    return render_template(template_filename, **context_dict)
+    return render_template("index.html")
+
+
+@app.route("/search-result?input_keyword=")
+def search_result_direct_access():
+    app.logger.info("search_result_direct_access(): GET /")
+    return "TOPページに戻ってキーワードを入力してください"
+
+
+@app.route("/search-result", methods=["GET"])
+def search_result():
+    app.logger.info("search_result(): GET /")
+
+    keyword = request.args.get("input_keyword", "")
+    products_list = []
+    search_products_result = amazon_api_client.search_products(keywords=keyword)
+    for asset in search_products_result:
+        products_list.append({"asin": asset.asin, "title": asset.title, "image_url": asset.images.large})
+    print(products_list)
+    if keyword:
+        return render_template("search-result.html", keyword=keyword)
+    else:
+        return "TOPページに戻ってキーワードを入力してください"
 
 
 if __name__ == "__main__":
