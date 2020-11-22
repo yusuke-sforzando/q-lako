bfa18b9e4f1aeecd831733d0bca6b5ea8bd3dd6c
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-22 22:47:31 +0900

    :sparkles: Improve main.py for revealed query

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def index():
 
 @app.route("/search", methods=["GET"])
 def search():
-    app.logger.info("search(): GET /search")
     template_filename = "search.html"
     products_list = []
     keyword = request.args.get("query", "")
+    app.logger.info("search(): GET /search?query={}".format(keyword))
     search_products_result = amazon_api_client.search_products(keywords=keyword)
     for product in search_products_result:
         products_list.append({"asin": product.asin, "title": product.title, "image_url": product.images.large})
@@ -30,18 +30,18 @@ def search():
         "keyword": keyword,
         "products_list": products_list
     }
-    return render_template("search.html", **context_dict)
+    return render_template(template_filename, **context_dict)
 
 
-@app.route("/registration-details", methods=["GET"])
-def registration_details():
-    app.logger.info("search(): GET /registration-details")
-    template_filename = "registration-details.html"
+@ app.route("/registration", methods=["GET"])
+def registration():
+    app.logger.info("search(): GET /registration")
+    template_filename = "registration.html"
     context_dict = {
         "subtitle": template_filename,
         "asin": request.args.get("asin")
     }
-    return render_template("registration-details.html", **context_dict)
+    return render_template(template_filename, **context_dict)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -15,7 +15,7 @@ def test_GET_search(test_client):
     assert response.status_code == 200
 
 
-def test_GET_registration_details(test_client):
-    response = test_client.get("/registration-details?asin=B07B7HG86W")
-    assert b"registration-details.html" in response.data
+def test_GET_registration(test_client):
+    response = test_client.get("/registration?asin=B07B7HG86W")
+    assert b"registration.html" in response.data
     assert response.status_code == 200
