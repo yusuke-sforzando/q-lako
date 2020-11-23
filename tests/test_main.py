fe161090af68dd1fe48974fbe856e2e526aa92cf
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-23 23:15:02 +0900

    :+1: Add test_GET_index

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,12 @@ def test_client():
     return app.test_client()
 
 
+def test_GET_index(test_client):
+    response = test_client.get("/")
+    assert b"index.html" in response.data
+    assert response.status_code == 200
+
+
 def test_GET_search(test_client):
     response = test_client.get("/search?query=サーカスTC")
     assert b"search.html" in response.data
