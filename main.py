2f5b41ec09976e0f0e00010b1a49e5033a731d25
yusuke <67737076+yusuke-sforzando@users.noreply.github.com>
2020-11-27 22:27:50 +0900

    :construction: Fix conflict

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # !/usr/bin/env python3
 
-from flask import request, render_template, url_for
+from amazon.exception import AmazonException
+from flask import request, render_template, url_for, session
 
-from __init__ import app
+from __init__ import app, amazon_api_client
 from flash_message import FlashMessage, FlashCategories
 
 
@@ -14,16 +15,48 @@ def index():
 
 @app.route("/search", methods=["GET"])
 def search():
-    app.logger.info(f"search(): GET {request.full_path}")
-    keyword = request.args.get("query", None)
-    if keyword:
-        context_dict = {
-            "subtitle": f"Search Result for {keyword}",
-            "keyword": keyword,
-        }
+    app.logger.info(f"search(): GET /{request.full_path}")
+    context_dict = {
+        "subtitle": "Search results for {{ keyword }}",
+        "keyword": request.args.get("query", None)
+    }
+    if context_dict["keyword"]:
+        if session.get("product_list", ""):
+            context_dict["product_list"] = session["product_list"]
+            context_dict["item_hits"] = len(session["product_list"])
+        else:
+            try:
+                session["product_list"] = amazon_api_client.search_products(keywords=context_dict["keyword"])
+                context_dict["product_list"] = session["product_list"]
+                context_dict["item_hits"] = len(session["product_list"])
+            except AmazonException as ae:
+                app.logger.error(f"{ae}")
+                raise ae
         return render_template("search.html", **context_dict)
     else:
         return FlashMessage.show_with_redirect("Enter any keywords.", FlashCategories.WARNING, url_for("index"))
+
+
+@ app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == "GET":
+        app.logger.info(f"registration: GET {request.full_path}")
+        return render_template("index.html", message="Enter any keywords.")
+    else:
+        app.logger.info(f"registration: POST {request.full_path}")
+    app.logger.debug(f"{request.form=}")
+    context_dict = {
+        "subtitle": "a service that displays detailed information about the item."
+    }
+    asin = request.form.get("asin", "")
+    if asin:
+        for product in session["product_list"]:
+            if product.asin == asin:
+                context_dict["product"] = product
+                # TODO: Convert Asset() from product.
+    else:
+        return render_template("index.html", message="Enter any keywords.")
+    return render_template("registration.html", **context_dict)
 
 
 @app.route("/register_airtable", methods=["POST"])
